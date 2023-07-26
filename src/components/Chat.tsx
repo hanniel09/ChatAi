bbfc15a502c59e5e67b62b8db7057b2ac049fb47
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-07-26 17:59:18 -0300

    Final Project

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -5,41 +5,44 @@ import { Button } from "./ui/button";
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "./ui/card";
 import { Input } from "./ui/input";
 import { useChat } from 'ai/react'
+import { ScrollArea } from "./ui/scroll-area";
 
 export function Chat(){
   const { messages, input, handleInputChange, handleSubmit } = useChat()
 
   return(
-   <Card className="w-[440px] h-[700px] grid grid-rows-[min-content_1fr_min-content]">
+   <Card className="w-[440px]">
       <CardHeader>
         <CardTitle>Chat AI</CardTitle>
         <CardDescription>Using Vercel SDK to create a chat bot.</CardDescription>
       </CardHeader>
-      <CardContent className="space-y-4">
-        {messages.map(message => {
-          return(
-          <div key={message.id} className="flex gap-3 text-slate-600 text-sm">
-            {message.role == 'user' && (
-              <Avatar>
-                <AvatarFallback>HV</AvatarFallback>
-                <AvatarImage src="https://github.com/hanniel09.png" />
-              </Avatar>
-          )}  
-            {message.role == 'assistant' && (
-              <Avatar>
-              <AvatarFallback>RS</AvatarFallback>
-              <AvatarImage src="https://github.com/rocketseat.png" />
-            </Avatar>
-          )}
-          <p className="leading-relaxed">
-            <span className="block font-bold text-slate-700">
-              {message.role == 'user' ? 'Usuário': 'AI'}:
-            </span>
-            {message.content}
-          </p>
-        </div>
-        )
-      })}
+      <CardContent>
+        <ScrollArea className="h-[600px] w-full pr-4">
+            {messages.map(message => {
+              return(
+              <div key={message.id} className="flex gap-3 text-slate-600 text-sm mt-2">
+                {message.role == 'user' && (
+                  <Avatar>
+                    <AvatarFallback>HV</AvatarFallback>
+                    <AvatarImage src="https://github.com/hanniel09.png" />
+                  </Avatar>
+              )}  
+                {message.role == 'assistant' && (
+                  <Avatar>
+                  <AvatarFallback>RS</AvatarFallback>
+                  <AvatarImage src="https://github.com/openai.png" />
+                </Avatar>
+              )}
+              <p className="leading-relaxed">
+                <span className="block font-bold text-slate-700">
+                  {message.role == 'user' ? 'Usuário': 'AI'}:
+                </span>
+                {message.content}
+              </p>
+            </div>
+            )
+          })}
+        </ScrollArea>
       </CardContent>
       <CardFooter>
         <form className="w-full flex gap-2" onSubmit={handleSubmit}>
